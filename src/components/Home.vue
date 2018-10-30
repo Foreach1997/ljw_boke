3602b3e52f1c3a2b33decc0ec03d94882cc2b86b
xiaoluo <473820041@qq.com>
2018-10-30 23:04:39 +0800

    1.0

---
+++ src/components/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="layui-container">
+    <div class="layui-row layui-col-space10">
+      <div class="layui-col-md3 layui-col-space5">
+        <h1>hellow</h1>
+      </div>
+      <div class="layui-col-md9 layui-col-space5">
+        <h1>hellow</h1>
+      </div>
+      <div class="layui-col-md12 layui-col-space5">
+        <h1>hellow</h1>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: ''
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,34 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+import Home from '../components/Home'
+import Journal from '../components/Journal'
+import Author from '../components/Author'
+import Community from '../components/Community'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/home',
+      component: Home
+    },
+    {
+      path: '/journal',
+      component: Journal
+    },
+    {
+      path: '/author',
+      component: Author
+    },
+    {
+      path: '/community',
+      component: Community
+    },
+    {
+      path:'/',
+      redirect:'/home'
+    }
+  ]
+})
