3c10e1edd614314c6cb51d1b803988f68260f817
xiaoluo <473820041@qq.com>
2018-10-31 15:39:00 +0800

    修改页面

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,10 @@
   <div>
   <div>
     <ul class="layui-nav layui-bg-blue" lay-filter="">
-      <li id="home" class="layui-nav-item layui-this" @click="home"><router-link to="/home">首页</router-link></li>
-      <li id="journal" class="layui-nav-item" @click="journal"><router-link to="/journal">日志</router-link></li>
-      <li id="community" class="layui-nav-item" @click="community"><router-link to="/community">社区</router-link></li>
-      <li id="author" class="layui-nav-item" @click="author"><router-link to="/author">关于作者</router-link></li>
+      <li id="home" class="layui-nav-item layui-this"><router-link to="/home">首页</router-link></li>
+      <li id="journal" class="layui-nav-item"><router-link to="/journal">日志</router-link></li>
+      <li id="community" class="layui-nav-item"><router-link to="/community">社区</router-link></li>
+      <li id="author" class="layui-nav-item"><router-link to="/author">关于作者</router-link></li>
     </ul>
   </div>
   <div id="s" style="width: 100%;height: 110px">
